05a24951119d5113d453a00b449f88e547ede89e
dhruba-star <indiracgps@gmail.com>
2025-04-13 07:01:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,7 @@ def factorize():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run()
+    import os
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
